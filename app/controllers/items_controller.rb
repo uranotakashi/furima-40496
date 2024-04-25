98886aa752eea9c66a31dacef216b640b3f195db
uranotakashi <takashiurano1@outlook.jp>
2024-04-25 20:38:19 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  
   def index
-    @items = Item.all
-    @items = Item.order("created_at DESC")
+    @items = Item.all.order("created_at DESC")
   end
   
   def new
